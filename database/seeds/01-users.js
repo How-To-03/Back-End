c54bd11bcc0f3249c75911fde561b81470dba0bb
Harry Merritt <harry@merritt.es>
2020-05-28 13:34:22 +0100

    update seed data * add two test accounts

--- database/seeds/01-users.js
+++ database/seeds/01-users.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcryptjs");
 
 exports.seed = function (knex) {
     return knex("users").insert([
-        { id: 1, username: "test", password: bcrypt.hashSync("test", 6) }
+        { id: 1, username: "test", password: bcrypt.hashSync("test", 6) },
+        { id: 2, username: "test2", password: bcrypt.hashSync("test2", 6) }
     ]);
 };
--- database/seeds/03-post_likes.js
+++ database/seeds/03-post_likes.js
@@ -1,6 +1,7 @@
 exports.seed = function (knex) {
     return knex("post_likes").insert([
         { user_id: 1, post_id: 1 },
-        { user_id: 1, post_id: 2 },
+        { user_id: 2, post_id: 1 },
+        { user_id: 2, post_id: 2 },
     ]);
 };
