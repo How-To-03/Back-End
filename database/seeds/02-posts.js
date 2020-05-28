3e658dce3f80f466db40788cf1e8ec7d7d1f27dc
Harry Merritt <harry@merritt.es>
2020-05-28 01:05:53 +0100

    add seed data for post_likes table

--- database/seeds/02-posts.js
+++ database/seeds/02-posts.js
@@ -1,5 +1,3 @@
-const bcrypt = require("bcryptjs");
-
 exports.seed = function (knex) {
     return knex("posts").insert([
         { id: 1, user_id: 1, content: "_INSERT AMAZING LIFE HACK_", image: "https://res.cloudinary.com/dicdxigq4/image/upload/v1590538257/sample.jpg", likes: 8 },
---
+++ database/seeds/03-post_likes.js
@@ -0,0 +1,6 @@
+exports.seed = function (knex) {
+    return knex("post_likes").insert([
+        { user_id: 1, post_id: 1 },
+        { user_id: 1, post_id: 2 },
+    ]);
+};
